77ea57caae4760ea900d86f67ab500d06561e357
aureldemiraj <58442040+aureldemiraj@users.noreply.github.com>
2020-03-19 00:32:21 +0100

    main completed

--- ds.java
+++ ds.java
@@ -19,6 +19,18 @@ public class ds {
                     default: System.out.println("CorrectWay: <unit(lines,words,letters,vowels,symbols,consonants,sentences)> <text>");
                 }
             }
+             else if (args[0].equalsIgnoreCase("case")){
+                switch (args[1].toLowerCase())
+                {
+                    case "lower": System.out.println("Lower: " + c2.lower(args[2]));break;
+                    case "upper": System.out.println("Upper: " + c2.upper(args[2]));break;
+                    case "capitalize": System.out.println("Capitalize: " + c2.capitalize(args[2]));break;
+                    case "inverse": System.out.println("Inverse: " + c2.inverse(args[2]));break;
+                    case "alternating": System.out.println("Alternating: " + c2.alternating(args[2]));break;
+		            case "sentence": System.out.println("Sentence: " + c2.sentence(args[2]));break;
+                    default: System.out.println("CorrectWay: <case(lower,upper,capitalize,inverse,alternating,sentence)> <text>");
+                }
+            }
             else if(args[0].equalsIgnoreCase("tap-code")){
                 switch (args[1].toLowerCase())
                 {
