f49aa1f033e06497eb3da801877b87a97b8653ca
durajetz <durajet.mustafa@studentet.uni-pr.edu>
2020-06-01 16:24:32 +0200

    Create jwt token when logged in

--- testlogin.java
+++ testlogin.java
@@ -4,18 +4,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Scanner;
 import java.nio.file.*;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.SecureRandom;
+import java.util.Base64;
 
-
-public class ds {
+public class Loginsystem {
 
     static final String pa = System.getProperty("user.home") + "\\" + "Desktop" + "\\";
-
-
-    public static void main(String[] args) throws Exception {
-        ruajpw(args[0]);
-//        checkpw(args[0]);
-//        deletepw(args[0]);
-    }
+    static final int iterations = 20*1000;
+    static final int saltLen = 32;
+    static final int desiredKeyLen = 256;
 
     public static void writeFile1(String path, String stringu) throws Exception {
         File file = new File(pa + path);
@@ -25,22 +25,46 @@ public class ds {
         writer.close();
     }
 
+    public static String getSaltedHash(String password) throws Exception {
+        byte[] salt = SecureRandom.getInstance("SHA1PRNG").generateSeed(saltLen);
+        return Base64.getEncoder().encodeToString(salt) + "$" + hash(password, salt);
+    }
+
+    public static boolean check(String password, String stored) throws Exception{
+        String[] saltAndHash = stored.split("\\$");
+        String hashOfInput = hash(password, Base64.getDecoder().decode(saltAndHash[0]));
+        return hashOfInput.equals(saltAndHash[1]);
+    }
+
+    public static String hash(String password, byte[] salt) throws Exception {
+        if (password == null || password.length() == 0)
+            throw new IllegalArgumentException("Empty passwords are not supported.");
+        SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        SecretKey key = f.generateSecret(new PBEKeySpec(
+                password.toCharArray(), salt, iterations, desiredKeyLen));
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
     public static void ruajpw(String argumenti) throws Exception {
-        Password pas = new Password();
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Jepni fjalkalimin: ");
+        System.out.print("Jepni passwordin: ");
         String passi = input.nextLine();
 
         Pattern fileExtnPtrn = Pattern.compile("(?=.*[0-9])(?=.*[a-z])(?=.*[@#$%^&+=_.!&-])(?=\\S+$).{6,}");
         Matcher mtch = fileExtnPtrn.matcher(passi);
+
+        if(passi.isEmpty()){
+            System.out.println("Gabim: Fjalkalimi nuk duhet te jet i zbrasur");
+            System.exit(1);
+        }
 
         if (!mtch.matches()) {
             System.out.println("\nGabim: Fjalkalimi se paku duhet te permbaj " + "\n/ 6 karaktere" + "/ Nje numer" + " / Nje simbol");
             System.exit(1);
         }
 
-        System.out.print("Perserit fjalkalimin: ");
+        System.out.print("Perseritni passwordin: ");
         String passi1 = input.nextLine();
 
         if (!passi.equals(passi1)) {
@@ -49,14 +73,14 @@ public class ds {
         }
 
         input.close();
-        String hashi = pas.getSaltedHash(passi);
+        String hashi = getSaltedHash(passi);
 
         String komplet = argumenti + " " + hashi;
 
         writeFile1("database.txt", komplet);
     }
 
-    public static void deletepw(String s) throws Exception {
+    public static void deletepw(String argumenti) throws Exception {
         File inputFile = new File(pa + "database.txt");
         File tempFile = new File(pa + "tempfile.txt");
 
@@ -66,7 +90,7 @@ public class ds {
         String currentLine;
 
         while((currentLine = reader.readLine()) != null) {
-            boolean bool = currentLine.split(" ")[0].equals(s);
+            boolean bool = currentLine.split(" ")[0].equals(argumenti);
             if(bool) continue;
             writer.write(currentLine + System.getProperty("line.separator"));
         }
@@ -77,10 +101,16 @@ public class ds {
     }
 
     public static void checkpw(String argumenti) throws Exception {
-        Password pas = new Password();
         Scanner input = new Scanner(System.in);
         System.out.print("Jepni fjalkalimin: ");
         String passi = input.nextLine();
+
+        String content = Key.readFile(pa + "database.txt");
+
+        if(!content.contains(argumenti)){
+            System.out.println("Gabim: Shfrytezuesi ose fjalekalimi i gabuar.");
+            System.exit(1);
+        }
 
         try {
             String line = "";
@@ -89,22 +119,21 @@ public class ds {
                 boolean bool = line.split(" ")[0].equals(argumenti);
 
                 if (bool) {
-                    boolean paswordi = pas.check(passi, line.split("\\s+|\n")[1]);
+                    boolean paswordi = check(passi, line.split("\\s+|\n")[1]);
                     if (paswordi == true) {
-                        System.out.println("Passwordi korrekt");
+                        System.out.println(JWT.krijojwt(argumenti));
                     } else {
-                        System.out.println("inkorrekt");
+                        System.out.println("Gabim: Shfrytezuesi ose fjalekalimi i gabuar.");
+                        System.exit(1);
                     }
                     break;
                 }
 
             }
             br.close();
-        } catch (FileNotFoundException ex) {
-            System.err.println("Gabim: Fajlli '" + argumenti + "'" + " nuk ekziston.");
         } catch (Exception e) {
-            System.err.println("Gabim: fajlli '" + argumenti + "' nuk ekziston.");
+            System.err.println("Gabim: celesi '" + argumenti + "' nuk ekziston.");
         }
-
     }
+
 }
