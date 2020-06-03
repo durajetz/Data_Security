d5e7935db3a662c5066f5f78d188395233ddcc46
Durajet Mustafa <durajet.mustafa@studentet.uni-pr.edu>
2020-06-03 17:38:04 +0200

    Updated: arguments validation
    And new commands(login,status) added

--- Projekti_Siguri/src/main/java/Arguments.java
+++ Projekti_Siguri/src/main/java/Arguments.java
@@ -1,160 +1,178 @@
 class Arguments {
-    Count c = new Count();
-    Tapcode t = new Tapcode();
-    Case c2 = new Case();
-    Key k1 = new Key();
 
-    public void count(String[] args) {
+    public static void count(String[] args) {
         if (args.length != 3) {
-            System.out.println("Wrong number of arguments provided!");
+            System.out.println("Error: Invalid number of arguments provided!\nExpect: <count> <function> <text>");
             System.exit(1);
         }
         switch (args[1].toLowerCase()) {
             case "lines":
-                System.out.println("Lines: " + c.lines(args[2]));
+                System.out.println("Lines: " + Count.lines(args[2]));
                 break;
             case "words":
-                System.out.println("Words: " + c.words(args[2]));
+                System.out.println("Words: " + Count.words(args[2]));
                 break;
             case "letters":
-                System.out.println("Letters: " + c.letters(args[2]));
+                System.out.println("Letters: " + Count.letters(args[2]));
                 break;
             case "vowels":
-                System.out.println("Vowels: " + c.vowels(args[2]));
+                System.out.println("Vowels: " + Count.vowels(args[2]));
                 break;
             case "symbols":
-                System.out.println("Symbols: " + c.symbols(args[2]));
+                System.out.println("Symbols: " + Count.symbols(args[2]));
                 break;
             case "consonants":
-                System.out.println("Consonants: " + c.consonants(args[2]));
+                System.out.println("Consonants: " + Count.consonants(args[2]));
                 break;
             case "sentences":
-                System.out.println("Sentences: " + c.sentences(args[2]));
+                System.out.println("Sentences: " + Count.sentences(args[2]));
                 break;
             default:
-                System.out.println("CorrectWay: <unit(lines,words,letters,vowels,symbols,consonants,sentences)> <text>");
+                System.out.println("Expect: <unit> <lines|words|letters|vowels|symbols|consonants|sentences> <text>");
         }
     }
 
-    public void tapcode(String[] args) {
+    public static void tapcode(String[] args) {
         if (args.length != 3) {
-            System.err.println("Wrong number of arguments provided!");
+            System.err.println("Error: Invalid number of arguments provided!\nExpect: <tap-code> <function> <text>");
             System.exit(1);
         }
         switch (args[1].toLowerCase()) {
             case "encode":
-                t.encode(args[2]);
+                Tapcode.encode(args[2]);
                 break;
             case "decode":
-                t.decode(args[2]);
+                Tapcode.decode(args[2]);
                 break;
             default:
-                System.out.println("CorrectWay: <code(encode,decode)> <text>");
+                System.out.println("Expect: <code> <encode|decode> <text>");
         }
     }
 
-    public void casecmd(String[] args) {
+    public static void casecmd(String[] args) {
         if (args.length != 3) {
-            System.err.println("Wrong number of arguments provided!");
+            System.err.println("Error: Invalid number of arguments provided!\nExpect: <case> <function> <text>");
             System.exit(1);
         }
         switch (args[1].toLowerCase()) {
             case "lower":
-                System.out.println("Lower: " + c2.lower(args[2]));
+                System.out.println("Lower: " + Case.lower(args[2]));
                 break;
             case "upper":
-                System.out.println("Upper: " + c2.upper(args[2]));
+                System.out.println("Upper: " + Case.upper(args[2]));
                 break;
             case "capitalize":
-                System.out.println("Capitalize: " + c2.capitalize(args[2]));
+                System.out.println("Capitalize: " + Case.capitalize(args[2]));
                 break;
             case "inverse":
-                System.out.println("Inverse: " + c2.inverse(args[2]));
+                System.out.println("Inverse: " + Case.inverse(args[2]));
                 break;
             case "alternating":
-                System.out.println("Alternating: " + c2.alternating(args[2]));
+                System.out.println("Alternating: " + Case.alternating(args[2]));
                 break;
             case "sentence":
-                System.out.println("Sentence: " + c2.sentence(args[2]));
+                System.out.println("Sentence: " + Case.sentence(args[2]));
                 break;
             default:
-                System.out.println("CorrectWay: <case(lower,upper,capitalize,inverse,alternating)> <text>");
+                System.out.println("Expect: <case> <lower|upper|capitalize|inverse|alternating> <text>");
         }
     }
 
-    public void usercreate(String[] args) throws Exception {
+    public static void usercreate(String[] args) throws Exception {
         if (args.length != 2) {
-            System.out.println("Wrong number of arguments provided!");
+            System.out.println("Error: Invalid number of arguments provided!\nExpect: <create-user> <user>");
             System.exit(1);
         }
-        k1.saveFile(args[1]);
+        Key.saveFile(args[1]);
     }
 
-    public void userdelete(String[] args) throws Exception {
+    public static void userdelete(String[] args) throws Exception {
         if (args.length != 2) {
-            System.out.println("Wrong number of arguments provided!");
+            System.out.println("Error: Invalid number of arguments provided!\nExpect: <delete-user> <user>");
             System.exit(1);
         }
-        k1.deleteFile(args[1]);
+        Key.deleteFile(args[1]);
     }
 
-    public void exportkey(String[] args) throws Exception {
+    public static void exportkey(String[] args) throws Exception {
         if (args.length == 3) {
             switch (args[1].toLowerCase()) {
                 case "public":
-                    k1.exportFile(args[2], ".pub.xml", args[1]);
+                    Key.exportFile(args[2], ".pub.xml", args[1]);
                     break;
                 case "privat":
-                    k1.exportFile(args[2], ".xml", args[1]);
+                    Key.exportFile(args[2], ".xml", args[1]);
                     break;
                 default:
-                    System.out.println("CorrectWay: <public|privat> <name> [file]");
+                    System.out.println("Expect: <public|privat> <user> [file]");
             }
         } else if (args.length == 4) {
             switch (args[1].toLowerCase()) {
                 case "public":
-                    k1.copyFile(args[2], ".pub.xml", args[3], args[1]);
+                    Key.copyFile(args[2], ".pub.xml", args[3], args[1]);
                     break;
                 case "privat":
-                    k1.copyFile(args[2], ".xml", args[3], args[1]);
+                    Key.copyFile(args[2], ".xml", args[3], args[1]);
                     break;
                 default:
-                    System.out.println("CorrectWay: <public|privat> <name> [file]");
+                    System.out.println("Expect: <public|privat> <user> [file]");
             }
         } else {
-            System.out.println("Wrong number of arguments provided!");
+            System.out.println("Error: Invalid number of arguments provided!\nExpect: <export-key> <public|privat> <user> [file]");
             System.exit(1);
         }
 
     }
 
-    public void importkey(String[] args) throws Exception {
+    public static void importkey(String[] args) throws Exception {
         if (args.length != 3) {
-            System.out.println("Wrong number of arguments provided!");
+            System.out.println("Error: Invalid number of arguments provided!\nExpect: <import-key> <(user.home)/file> <user>");
             System.exit(1);
         }
-        k1.importKey(args[1], args[2]);
+        Key.importKey(args[1], args[2]);
     }
 
-    public void writemessage(String[] args) throws Exception {
-        if (args.length < 3) {
-            System.err.println("Wrong number of arguments provided!");
-            System.exit(1);
-        }
-
+    public static void writemessage(String[] args) throws Exception {
         if (args.length == 3) {
-            k1.writeMessage(args[1], args[2], "");
-        } else {
-            k1.writeMessage(args[1], args[2], args[3]);
+            Key.writeMessage(args[1], args[2], null,null);
+        } else if (args.length == 4){
+            Key.writeMessage(args[1],args[2],args[3],null);
+        }else if (args.length == 5){
+            if (args[3].equals("--sender")){
+                Key.writeMessage(args[1],args[2],null,args[4]);
+            }else System.out.println("Error: Invalid number of arguments provided!\nExpect: --sender <token>");
+        }else if(args.length == 6){
+            if(args[4].equals("--sender")){
+                Key.writeMessage(args[1],args[2],args[3],args[5]);
+            }else System.out.println("Error: Invalid number of arguments provided!\nExpect: [file] --sender <token>");
+        }else {
+            System.err.println("Error: Invalid number of arguments provided!\nExpect: <user> <message> [file] [--sender <token>]");
+            System.exit(1);
         }
     }
 
-    public void readmessage(String[] args) throws Exception {
+    public static void readmessage(String[] args) throws Exception {
         if (args.length != 2) {
-            System.out.println("Wrong number of arguments provided!");
+            System.out.println("Error: Invalid number of arguments provided!\nExpect: <read-message> <encrypted|path>");
             System.exit(1);
         }
-        k1.readMessage(args[1]);
+        Key.readMessage(args[1]);
+    }
+
+    public static void login(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println("Error: Invalid number of arguments provided!\nExpect: <login> <user>");
+            System.exit(1);
+        }
+        Loginsystem.checkpw(args[1]);
+    }
+
+    public static void status(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println("Error: Invalid number of arguments provided!\nExpect: <status> <token>");
+            System.exit(1);
+        }
+        JWT.verifikojwt(args[1]);
     }
 
 }
