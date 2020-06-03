d0e10c0adcf3a345da7ccd5bf754650e984e5fed
Durajet Mustafa <durajet.mustafa@studentet.uni-pr.edu>
2020-06-03 17:41:10 +0200

    Updated: converting type of methods to static

--- Projekti_Siguri/src/main/java/Count.java
+++ Projekti_Siguri/src/main/java/Count.java
@@ -1,6 +1,6 @@
 public class Count {
-        // COUNT LINES
-        public int lines(String s) {
+
+        public static int lines(String s) {
             if (s == null || s.isEmpty()) {
                 return 0;
             }
@@ -8,8 +8,7 @@ public class Count {
             return lines.length;
         }
 
-        // COUNT WORDS
-        public int words(String s) {
+        public static int words(String s) {
             if (s == null || s.isEmpty()) {
                 return 0;
             }
@@ -18,8 +17,7 @@ public class Count {
             return words.length;
         }
 
-        // COUNT LETTERS
-        public int letters(String s) {
+        public static int letters(String s) {
             int count = 0;
             for (char c : s.toCharArray()) {
                 if (Character.isLetter(c))
@@ -28,8 +26,7 @@ public class Count {
             return count;
         }
 
-        // COUNT SYMBOLS
-        public int symbols(String s){
+        public static int symbols(String s){
             int count2 = 0;
             for (int i = 0; i < s.length(); i++) {
                 char ch = s.charAt(i);
@@ -40,8 +37,7 @@ public class Count {
             return count2;
         }
 
-        // COUNT VOWELS
-        public int vowels(String s) {
+        public static int vowels(String s) {
 
             int count1 = 0;
             s = s.toLowerCase();
@@ -52,8 +48,7 @@ public class Count {
             return count1;
         }
 
-        // COUNT CONSONANTS
-        public int consonants(String s){
+        public static int consonants(String s){
             int count3 = 0;
             s = s.toLowerCase();
             for (char ch1 : s.toCharArray()){
@@ -66,8 +61,7 @@ public class Count {
             return count3;
         }
 
-        // COUNT SENTENCES
-        public int sentences(String s){
+        public static int sentences(String s){
             String[] sentence = s.split("[!?.:]+");
             return sentence.length;
         }
